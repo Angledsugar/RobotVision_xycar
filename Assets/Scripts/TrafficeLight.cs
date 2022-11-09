1416b0cc703a0b63bd36ed6906a27239f94f2a64
Angledsugar <angledsugar@gmail.com>
2022-11-09 19:30:36 +0900

    v1.2

--- Assets/Scripts/TrafficeLight.cs
+++ Assets/Scripts/TrafficeLight.cs
@@ -6,19 +6,19 @@ public class TrafficeLight : MonoBehaviour
 {
     public GameObject Red1;
     public GameObject Red2;
-    public GameObject Red3;
+    // public GameObject Red3;
     
     public GameObject Yellow1;
     public GameObject Yellow2;
-    public GameObject Yellow3;
+    // public GameObject Yellow3;
     
     public GameObject Green1;
     public GameObject Green2;
-    public GameObject Green3;
+    // public GameObject Green3;
 
-    private float TimeLeft = 15.0f;
+    private float TimeLeft = 10.0f;
     private float nextTime = 0.0f;
-    int i = 0;
+    int i = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -35,87 +35,89 @@ public class TrafficeLight : MonoBehaviour
             if(i == 1){
                 Red1.SetActive(true); //
                 Red2.SetActive(false);
-                Red3.SetActive(false);
+                // Red3.SetActive(false);
 
                 Yellow1.SetActive(true);
                 Yellow2.SetActive(true);
-                Yellow3.SetActive(true);
+                // Yellow3.SetActive(true);
                 
                 Green1.SetActive(false); //
                 Green2.SetActive(true);
-                Green3.SetActive(true);
+                // Green3.SetActive(true);
             }
 
             if(i == 2){
                 Red1.SetActive(true); //
                 Red2.SetActive(false);
-                Red3.SetActive(false);
+                // Red3.SetActive(false);
 
                 Yellow1.SetActive(false); //
                 Yellow2.SetActive(true);
-                Yellow3.SetActive(true);
+                // Yellow3.SetActive(true);
                 
                 Green1.SetActive(true);
                 Green2.SetActive(true);
-                Green3.SetActive(true);
+                // Green3.SetActive(true);
             }
 
             if(i == 3){
                 Red1.SetActive(false);
                 Red2.SetActive(true); //
-                Red3.SetActive(false);
+                // Red3.SetActive(false);
 
                 Yellow1.SetActive(true);
                 Yellow2.SetActive(true); //
-                Yellow3.SetActive(true);
+                // Yellow3.SetActive(true);
                 
                 Green1.SetActive(true);
                 Green2.SetActive(false); //
-                Green3.SetActive(true);
+                // Green3.SetActive(true);
             }
 
             if(i == 4){
                 Red1.SetActive(false);
                 Red2.SetActive(true); //
-                Red3.SetActive(false);
+                // Red3.SetActive(false);
 
                 Yellow1.SetActive(true);
                 Yellow2.SetActive(false); //
-                Yellow3.SetActive(true);
+                // Yellow3.SetActive(true);
                 
                 Green1.SetActive(true);
                 Green2.SetActive(true); //
-                Green3.SetActive(true);
-            }
+                // Green3.SetActive(true);
 
-            if(i == 5){
-                Red1.SetActive(false);
-                Red2.SetActive(false);
-                Red3.SetActive(true); //
-
-                Yellow1.SetActive(true);
-                Yellow2.SetActive(true);
-                Yellow3.SetActive(true); //
-                
-                Green1.SetActive(true);
-                Green2.SetActive(true);
-                Green3.SetActive(false); //
-            }
-
-            if(i == 6){
-                Red1.SetActive(false);
-                Red2.SetActive(false);
-                Red3.SetActive(true); //
-
-                Yellow1.SetActive(true);
-                Yellow2.SetActive(true);
-                Yellow3.SetActive(false); //
-                
-                Green1.SetActive(true);
-                Green2.SetActive(true);
-                Green3.SetActive(true); //
                 i = -1;
             }
+
+            // if(i == 5){
+            //     Red1.SetActive(false);
+            //     Red2.SetActive(false);
+            //     // Red3.SetActive(true); //
+
+            //     Yellow1.SetActive(true);
+            //     Yellow2.SetActive(true);
+            //     // Yellow3.SetActive(true); //
+                
+            //     Green1.SetActive(true);
+            //     Green2.SetActive(true);
+            //     // Green3.SetActive(false); //
+            // }
+
+            // if(i == 6){
+            //     Red1.SetActive(false);
+            //     Red2.SetActive(false);
+            //     // Red3.SetActive(true); //
+
+            //     Yellow1.SetActive(true);
+            //     Yellow2.SetActive(true);
+            //     // Yellow3.SetActive(false); //
+                
+            //     Green1.SetActive(true);
+            //     Green2.SetActive(true);
+            //     // Green3.SetActive(true); //
+            //     i = -1;
+            // }
 
             i ++;
 
